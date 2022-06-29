b6640fb1d19d43c8a6018e4988ee7263b40350ce
Christopher Kenny <ctk44@cornell.edu>
2022-06-28 22:29:36 -0400

    colorful when out of range

--- R/io.R
+++ R/io.R
@@ -4,6 +4,7 @@
 #' @param init_plan Plan to initialize with.
 #' @param ndists Number of districts to draw if `init_plan` is not supplied.
 #' @param palette Color palette to fill shapes with. Default is Polychrome 36.
+#' @param pop_tol the population tolerance.
 #' @param save_path Output path to save progress to.
 #'
 #' @return Shiny app
@@ -14,9 +15,9 @@
 #'   draw(dc, dc$ward)
 #' }
 #'
-draw <- function(shp, init_plan, ndists, palette,
+draw <- function(shp, init_plan, ndists, palette, pop_tol = 0.05,
                  save_path = tempfile(fileext = '.baf')) {
-  palette <- as.character(palette)
+
 
   if (missing(shp)) {
     stop('`shp` missing, but required.')
@@ -35,9 +36,14 @@ draw <- function(shp, init_plan, ndists, palette,
   if (missing(palette)) {
     palette <- grDevices::palette.colors(n = ndists, 'Polychrome 36')
   }
+  palette <- as.character(palette)
 
   shp$redistio_id <- seq_len(length.out = nrow(shp))
   shp$redistio_curr_plan <- init_plan
+
+  tgt_pop <- sum(shp$pop) / ndists
+  min_pop <- ceiling(tgt_pop * (1 - pop_tol))
+  max_pop <- floor(tgt_pop * (1 + pop_tol))
 
   ui <- shiny::fluidPage(
     title = 'redistio',
@@ -72,6 +78,7 @@ draw <- function(shp, init_plan, ndists, palette,
         dplyr::group_by(.data$redistio_curr_plan) %>%
         dplyr::summarize(pop = sum(.data$pop))
     )
+    clicked <- shiny::reactiveValues(clickedMarker = NULL)
 
 
     bbox <- unname(sf::st_bbox(shp))
@@ -89,13 +96,21 @@ draw <- function(shp, init_plan, ndists, palette,
           # line colors
           stroke = TRUE, weight = 1, color = '#000000',
           # fill control
-          fillOpacity = 0.95, fillColor = ~pal()(shp$redistio_curr_plan)
+          fillOpacity = 0.95, fillColor = ~pal()(shp$redistio_curr_plan),
+          # label
+          label = ~shp$pop
         ) #%>%
       #leaflet::fitBounds(bbox[1], bbox[2], bbox[3], bbox[4])
     })
 
-    shiny::observe({
-      click <- input$map_shape_click
+    shiny::observeEvent(input$map_shape_click,{
+      clicked$map_shape_click <- input$map_shape_click
+    })
+
+    shiny::observeEvent(eventExpr = clicked$map_shape_click,
+                        {
+      click <- clicked$map_shape_click
+      clicked$map_shape_click <- NULL
       if (is.null(click)) {
         return(NULL)
       }
@@ -106,7 +121,9 @@ draw <- function(shp, init_plan, ndists, palette,
       values$tab_pop <- shp %>%
         dplyr::as_tibble() %>%
         dplyr::group_by(.data$redistio_curr_plan) %>%
-        dplyr::summarize(pop = sum(.data$pop))
+        dplyr::summarize(pop = sum(.data$pop)) %>%
+        dplyr::mutate(dev = .data$pop - round(tgt_pop)) %>%
+        dplyr::arrange(as.integer(.data$redistio_curr_plan))
 
       leaflet::leafletProxy('map') %>%
         leaflet::clearShapes() %>%
@@ -115,16 +132,27 @@ draw <- function(shp, init_plan, ndists, palette,
           # line colors
           stroke = TRUE, weight = 1, color = '#000000',
           # fill control
-          fillOpacity = 0.95, fillColor = ~pal()(shp$redistio_curr_plan)
+          fillOpacity = 0.95, fillColor = ~pal()(shp$redistio_curr_plan),
+          # label
+          label = ~shp$pop
         )
     })
 
     shiny::observe({
       output$tab_pop <- gt::render_gt({
         values$tab_pop %>%
-          gt::gt()  %>%
+          gt::gt() %>%
+          gt::tab_style(
+            style = gt::cell_fill(color = 'red'),
+            locations = gt::cells_body(
+              rows = .data$pop > max_pop | .data$pop < min_pop
+            )
+          ) %>%
           gt::cols_label(
             redistio_curr_plan = ''
+          ) %>%
+          gt::tab_footnote(
+            footnote = paste0('Population must be in [', min_pop, ', ', max_pop, '].')
           )
       })
     })
