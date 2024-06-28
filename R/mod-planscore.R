18c7eb5ead24150919716c6436b8f4e366d79555
Christopher Kenny <ctk44@cornell.edu>
2024-06-28 18:05:44 -0400

    support planscore

--- R/mod-planscore.R
+++ R/mod-planscore.R
@@ -2,14 +2,19 @@ planscoreUI <- function(id) {
   shiny::tagList(
     shiny::h4('Upload to Planscore'),
     # upload button
+    shiny::textInput(
+      inputId = shiny::NS(id, 'psdescription'),
+      label = 'Description for plan for PlanScore',
+      value = 'Plan from redistio'
+    ),
     shiny::actionButton(
       inputId = shiny::NS(id, 'upload'),
       label = 'Upload',
       icon = shiny::icon('upload')
     ),
     shiny::uiOutput(shiny::NS(id, 'planscoreURLs')),
-    shiny::uiOutput(shiny::NS(id, 'ingestBut'))#,
-    #shiny::uiOutput(shiny::NS(id, 'psSummary'))
+    shiny::uiOutput(shiny::NS(id, 'ingestBut')),
+    shiny::uiOutput(shiny::NS(id, 'psSummary'))
   )
 }
 
@@ -37,8 +42,12 @@ planscoreServer <- function(id, plan, shp) {
         dplyr::summarise() |>
         sf::st_write(f)
 
-      ps_link(as.character(planscorer::ps_upload_file(file = f)))
-      cat(ps_link())
+      planscorer::ps_upload_file(
+        file = f, description = input$psdescription
+      ) |>
+        as.character() |>
+        ps_link()
+      cat(ps_link(),'\n')
       upload_time(Sys.time())
 
       output$planscoreURLs <- shiny::renderUI({
@@ -51,24 +60,68 @@ planscoreServer <- function(id, plan, shp) {
       })
 
       output$ingestBut <- shiny::renderUI({
-        shiny::actionButton(shiny::NS(id, 'ingest'), label = 'Download results')
+        shiny::actionButton(shiny::NS(id, 'ingest'), label = 'Summarize results')
       })
     })
 
     shiny::observeEvent(input$ingest, {
       ps_info <- NULL
-      try({ps_info <- planscorer::ps_ingest(ps_link()[1])})
+      try({ps_info <- planscorer::ps_ingest(ps_link()[1])}, silent = TRUE)
       if (is.null(ps_info)) {
-        shiny::showNotification('Error ingesting results. Try again in a few seconds.')
-        cat('PlanScore ingest failed at ', Sys.time(), '.\n')
+        shiny::showModal(
+          shiny::modalDialog('Error ingesting results. Try again in a few seconds.')
+        )
+        message('PlanScore ingest failed at ', Sys.time(), '.\n')
         return()
       }
 
       ps_data(ps_info)
 
-      # output$psSummary <- shiny::renderUI({
-      #
-      # })
+      output$psSummary <- shiny::renderUI({
+        planscorer_summary(ps_info)
+      })
     })
   })
 }
+
+color_bias <- function(x, domain = c(-0.3, 0.3),
+                       oob = function(x) scales::oob_squish(x, range = domain),
+                       reverse = FALSE) {
+  pal <- ggredist::ggredist$partisan
+  if (reverse) pal <- rev(pal)
+  scales::col_numeric(
+    palette = as.character(pal),
+    domain = domain
+  )(oob(x))
+}
+
+planscorer_summary <- function(ps_info) {
+  vbs <- list(
+    bslib::value_box(
+      title = 'Efficiency gap',
+      value = scales::label_percent(accuracy = 0.1, suffix = 'pp')(-1 * ps_info$efficiency_gap[1]),
+      theme = bslib::value_box_theme(bg = color_bias(ps_info$efficiency_gap[1]))
+    ),
+    bslib::value_box(
+      title = 'Mean median',
+      value = scales::label_number(accuracy = 0.01)(ps_info$mean_median[1]),
+      theme = bslib::value_box_theme(bg = color_bias(ps_info$mean_median[1]))
+    ),
+    bslib::value_box(
+      title = 'Partisan bias (Symmetry)',
+      value = scales::label_number(accuracy = 0.01)(ps_info$partisan_bias[1]),
+      theme = bslib::value_box_theme(bg = color_bias(ps_info$partisan_bias[1]))
+    ),
+    bslib::value_box(
+      title = 'Declination',
+      value = scales::label_number(accuracy = 0.01)(ps_info$declination[1]),
+      theme = bslib::value_box_theme(bg = color_bias(ps_info$declination[1]))
+    )
+  )
+
+  bslib::layout_column_wrap(
+    width = "250px",
+    !!!vbs
+  )
+}
+
