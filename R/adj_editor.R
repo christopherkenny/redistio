fa4e27ecce84a22e1f707cd115e5cee365ea6f7a
Christopher T. Kenny <ctk44@cornell.edu>
2025-10-03 11:47:44 -0400

    use mapgl in adj_editor

--- R/adj_editor.R
+++ R/adj_editor.R
@@ -1,5 +1,21 @@
-adj_editor <- function(shp, adj = geomander::adjacency(shp), opts = redistio_options()) {
-
+#' Interactive Adjacency Graph Editing
+#'
+#' @param shp an `sf` tibble that you want to draw with
+#' @param adj a zero-indexed adjacency graph
+#' @param opts list of options. Default is `redistio_options()`
+#'
+#' @return Shiny app
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
+#'   adj_editor(dc)
+#' }
+adj_editor <- function(
+  shp,
+  adj = geomander::adjacency(shp),
+  opts = redistio_options()
+) {
   # defaults ----
   def_opts <- redistio_options()
 
@@ -10,17 +26,20 @@ adj_editor <- function(shp, adj = geomander::adjacency(shp), opts = redistio_opt
 
   shp$redistio_id <- as.character(seq_len(length.out = nrow(shp)))
 
-  edges_centers <- edge_center_df(shp, adj)
-
   # process shp components ----
   if (!sf::st_is_longlat(shp)) {
     shp <- sf::st_transform(shp, opts$crs %||% def_opts$crs)
   }
 
+  edges_centers <- edge_center_df(shp, adj)
+
   shp <- shp |>
     sf::st_make_valid()
 
   # User Interface ----
+
+  leaf_tiles <- opts$map_tiles %||% def_opts$map_tiles
+
   ui <- bslib::page_navbar(
     title = 'redistio',
     theme = bslib::bs_theme(preset = (opts$theme %||% def_opts$theme)),
@@ -28,32 +47,58 @@ adj_editor <- function(shp, adj = geomander::adjacency(shp), opts = redistio_opt
     # editor panel ----
     bslib::nav_panel(
       title = ' adj editor',
-      bslib::card( # interactive mapper
+      bslib::card(
+        # interactive mapper
+        id = 'map-card',
         full_screen = TRUE,
-        leaflet::leafletOutput(
+        mapgl::maplibreOutput(
           outputId = 'map',
           height = opts$leaflet_height %||% def_opts$leaflet_height,
         )
-      )
+      ),
     )
   )
 
   # Server ----
   server <- function(input, output, session) {
-    output$map <- leaflet::renderLeaflet({
-      base_map <- leaflet::leaflet(data = shp) |>
-        leaflet::addTiles() |>
-        # leaflet::addProviderTiles(leaflet::providers$CartoDB.Positron) |>
-        leaflet::addPolygons(
-          layerId = ~redistio_id,
-          weight = 1,
-          label = ~redistio_id,
-          group = 'shapes'
+    output$map <- mapgl::renderMaplibre({
+      base_map <- mapgl::maplibre(
+        bounds = shp,
+        style = leaf_tiles
+      ) |>
+        mapgl::add_source(
+          id = 'redistio',
+          data = shp,
+          promoteId = 'redistio_id'
+        ) |>
+        mapgl::add_source(
+          id = 'lines',
+          data = edges_centers$nb,
+          promoteId = 'redistio_lines'
+        ) |>
+        # mapgl::add_fill_layer(
+        #   source = 'redistio',
+        #   id = 'precinct_fill',
+        #   fill_color = discrete_palette(palette, init_plan),
+        #   fill_opacity = 0.9,
+        #   fill_outline_color = '#00000000'
+        # ) |>
+        mapgl::add_line_layer(
+          id = 'precinct_border',
+          source = 'redistio',
+          line_color = opts$border_color %||% def_opts$border_color,
+          line_width = 0.5
+        ) |>
+        mapgl::add_line_layer(
+          id = 'edges',
+          source = 'lines',
+          line_color = opts$border_color %||% def_opts$border_color,
+          line_width = 1
         )
+
       base_map
     })
   }
-
   # run app ----
   shiny::shinyApp(ui = ui, server = server)
 }
