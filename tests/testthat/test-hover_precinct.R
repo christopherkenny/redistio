a92a46f0baae008373d9855df77e23511850afb6
Christopher Kenny <ctk44@cornell.edu>
2026-01-07 12:47:56 -0500

    Create test-hover_precinct.R

---
+++ tests/testthat/test-hover_precinct.R
@@ -0,0 +1,7 @@
+test_that("`hover_precinct()` works", {
+  hov <- hover_precinct(dc, 1, pop = dplyr::starts_with('pop'), vap = dplyr::starts_with('vap'))
+
+  expect_type(hov, 'list')
+  expect_equal(length(hov), 3)
+  expect_named(hov, c('', 'pop', 'vap'))
+})
